648b80c8247184f08f8de67bbdcbfe33ad7ddefe
Chris Cochrane <cdevelc@gmail.com>
2016-10-25 17:56:30 -0400

    last login time fixup

--- controller/ulist/ulist.go
+++ controller/ulist/ulist.go
@@ -12,7 +12,10 @@ func BeforeFilter(q *q29.ReqRsp) bool {
 func Dashboard(q *q29.ReqRsp) {
 	var page struct {
 		Vw q29.View
+		LastLoginTime string
 	}
+	t, _ := time.Parse("2006-01-02 15:04:05", q.U.LastLoginTime)
+	page.LastLoginTime = fmt.Sprintf(t.Format("January 2, 2006 3:04PM"))
 	page.Vw.Template = "ulist/dashboard"
 	q29.Render(q, &page)
 }
